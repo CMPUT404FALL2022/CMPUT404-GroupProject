95c1b8dd93de202bb274ade3643c84d5b87652f8
KexunNiu <kexun@ualberta.ca>
2022-10-22 11:05:28 +0800

    Update user model

--- SocialDistribution/login/models.py
+++ SocialDistribution/login/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class users(models.Model):
+    username = models.CharField(min_length=3,max_length=12,blank=False, null=False)
+    password = models.CharField(min_length=6,max_length=18,blank=False, null=False)
+    uid = models.CharField()
+    type = models.CharField()
+
+    # create a unique id for each new user
+    # def generate_unique_id(self):
+    #     unique_id = uuid.uuid4()
+
+
+
