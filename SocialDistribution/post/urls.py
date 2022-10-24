5cc8e3a0ea58d37f29e7a1f348eb90c7b1bf7325
Wenhao Cao <wenhaocao@Wenhaos-MacBook-Pro.local>
2022-10-23 19:55:01 -0600

    add create_post

--- SocialDistribution/post/urls.py
+++ SocialDistribution/post/urls.py
@@ -4,5 +4,5 @@ urlpatterns = [
     path("", views.home_page,name="home-page"),
     path("posts", views.posts,name="posts-page"),
     
-    
+    path("create", views.create_post,name="create-page")
 ]
