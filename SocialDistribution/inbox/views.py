4fb56fb81043a7035ae70d39796614958e095703
Sun-ning1 <chinaisrising999@gmail.com>
2022-10-24 16:24:39 -0600

    1

--- SocialDistribution/inbox/views.py
+++ SocialDistribution/inbox/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 #from django.http import HttpResponse
 
 def create(request):
-    return render(request,'index.html')
+    return render(request,'indox.html')
     #return HttpResponse("You're looking at question.")
