8b09e206d48dbbba5ece46bd1400f6f12cb0f146
Wenhao Cao <wenhaocao@Wenhaos-MacBook-Pro.local>
2022-10-25 19:48:25 -0600

    update

--- SocialDistribution/post/views.py
+++ SocialDistribution/post/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .post_forms import post_form
 from .models import Post
+from authors.models import single_author
 
 import uuid
 
@@ -11,7 +12,9 @@ import uuid
 
 def home_page(request):
     posts = Post.objects.all()
-    
+    author = single_author.objects.get()
+
+
     return render(request, 'post/index.html',{
         'posts': Post.objects.all()
         
@@ -26,7 +29,7 @@ def create_post(request):
     if request.method == 'POST':
         form = post_form(request.POST)
         if form.is_valid():
-            new_post = Post(title = form.cleaned_data['title'],description = form.cleaned_data['description'],content = form.cleaned_data['content'])
+            new_post = Post(title = form.cleaned_data['title'],description = form.cleaned_data['description'],content = form.cleaned_data['content'],Categories = form.cleaned_data['Categories'])
             new_post.save()
     
             return HttpResponseRedirect('../home')
