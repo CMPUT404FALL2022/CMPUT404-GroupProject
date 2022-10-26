0ab8d95c53985248e491bfb8fc773ba9f6dd9f67
WenhaoCao0729 <wcao4@ualberta.ca>
2022-10-25 23:49:22 -0600

    setting update

--- SocialDistribution/SocialDistribution/settings.py
+++ SocialDistribution/SocialDistribution/settings.py
@@ -134,3 +134,10 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'SocialDistribution/static',
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
