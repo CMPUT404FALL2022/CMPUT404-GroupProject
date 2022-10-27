987d5cdd57dc0c184d2fe2ef8bb5bbf6e52fec1f
mengjuL991 <jaydenliaomengju@gmail.com>
2022-10-26 19:27:06 -0600

    yy

--- SocialDistribution/me/urls.py
+++ SocialDistribution/me/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.my_page)
+    path('me/<str:userId>/', views.my_page),
+    path('me/<str:userId>/myPosts/', views.my_post_page),
 ]
