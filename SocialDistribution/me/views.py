194fe35c7616619a7293225a2f79dea7dcec1010
Sun-ning1 <chinaisrising999@gmail.com>
2022-10-27 20:36:19 -0600

    myinfo

--- SocialDistribution/me/views.py
+++ SocialDistribution/me/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render
 from authors.models import single_author
-
+from post.models import Post
 # Create your views here.
 
-def my_page(request, userId):
-    return render(request,'me.html')
+# def my_page(request, userId):
+#     return render(request,'me.html')
 
-<<<<<<< Updated upstream
-def my_post_page(request, userId):
-    return render(request,'my_post.html')
-=======
+# def my_post_page(request, userId):
+#     return render(request,'my_post.html')
+
+def my_profile(request, userId):
+    all_posts = Post.objects.all()
+
     return render(request,'my_profile.html',{
         "all_posts": all_posts,
         "userId": userId
@@ -23,4 +25,3 @@ def myinfo(request, userId):
         "userId": userId
 
     })
->>>>>>> Stashed changes
