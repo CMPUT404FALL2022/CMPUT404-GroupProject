d1e5637c400b4517c6bdaa92cad02655ed8bd60d
Sun-ning1 <chinaisrising999@gmail.com>
2022-10-27 21:53:24 -0600

    Update urls.py

--- SocialDistribution/me/urls.py
+++ SocialDistribution/me/urls.py
@@ -5,11 +5,7 @@ urlpatterns = [
 
     path("authors/<str:userId>/profile", views.my_profile,name="profile-page"),
     path("authors/<str:userId>/information", views.myinfo,name="information-page"),
-<<<<<<< Updated upstream
     path("authors/<str:userId>/informationedit", views.myinfoedit,name="informationedit-page"),
-=======
-    path("authors/<str:userId>/information_edit", views.myinfoedit,name="informationedit-page"),
->>>>>>> Stashed changes
 
 ]
 
