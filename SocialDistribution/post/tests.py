5731e96135570b3c60b2fa1ef81af2140e52d08e
WenhaoCao0729 <wcao4@ualberta.ca>
2022-11-12 14:34:31 -0700

    new update

--- SocialDistribution/post/tests.py
+++ SocialDistribution/post/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
-
+from .models import Post
 # Create your tests here.
+class PostTestCase(TestCase):
+    def setUp(self):
+        post_a = Post(title='TestCase', content = 'TestCaseTest')
+        post_a.save()
+        print(post_a.title)
