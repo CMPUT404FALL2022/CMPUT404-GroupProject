4f4e2dee0ebceee9a3b17fe15812a75042f2d6c8
XuantongMa <xuantong@ualberta.ca>
2022-11-23 14:52:34 -0700

    debug myinfo

--- SocialDistribution/me/urls.py
+++ SocialDistribution/me/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("authors/<str:userId>/profile/<str:postId>", views.my_profile_modify,name="modify-post-page"),
     path("authors/<str:userId>/information", views.myinfo,name="information-page"),
     
-    # path("authors/<str:userId>/informationedit", views.myinfoedit,name="informationedit-page"),
+    path("authors/<str:userId>/informationedit", views.myinfoedit,name="informationedit-page"),
 
 ]
 
