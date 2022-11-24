7d364fd3ed38c3866922e3ca099d3a5ed4baef64
WenhaoCao0729 <wcao4@ualberta.ca>
2022-11-24 14:56:52 -0700

    make it better

--- SocialDistribution/SocialDistribution/settings.py
+++ SocialDistribution/SocialDistribution/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -155,3 +156,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
     
+django_on_heroku.settings(locals())
