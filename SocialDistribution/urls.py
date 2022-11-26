4bb86eba0b0fc608b135830aacbd0719d38c6098
WenhaoCao0729 <wcao4@ualberta.ca>
2022-11-26 16:36:58 -0700

    add api url

--- SocialDistribution/urls.py
+++ SocialDistribution/urls.py
@@ -18,8 +18,19 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 
-    
+api_view = get_schema_view(
+    openapi.Info(
+        title="Socialdistribution API",
+        default_version="1.0.0",
+        description="API documentation of APP"
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('service/',include("service.urls")),
@@ -28,5 +39,7 @@ urlpatterns = [
     path('',include("post.urls")),
     path('',include('me.urls')), 
     path('',include('inbox.urls')), 
+    path('swagger/', api_view.with_ui('swagger',
+                                         cache_timeout=0), name='schema-swagger-ui'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
