d034d060e64dee196e2b3d517e16a4561b5ebcac
WenhaoCao0729 <wcao4@ualberta.ca>
2022-11-30 18:52:47 -0700

    update

--- login/urls.py
+++ login/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.log_in, name="login"),
     path("login/", views.log_in, name="login"),
     path("signup/", views.sign_up, name="signup")
 ]
