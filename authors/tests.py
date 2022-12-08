22c8ec562857706f9d5ead770552e7d7f5178548
mengjuL991 <jaydenliaomengju@gmail.com>
2022-12-07 21:18:54 -0700

    one more test case for authors

--- authors/tests.py
+++ authors/tests.py
@@ -6,14 +6,20 @@ class single_author_TestCase(TestCase):
         print("First test setup")
         single_author.objects.create(username='TestAuthorCase1', password = 'TestAuthorCaseTest1')
         single_author.objects.create(username='TestAuthorCase2', password = 'TestAuthorCaseTest2')
-        
+        single_author.objects.create(username='TestAuthorCase3', password = 'TestAuthorCaseTest3', github = 'www.github.com', display_name = 'test3')
+
     def test_post_exist(self):
         case1 = single_author.objects.get(username = 'TestAuthorCase1')
         case2 = single_author.objects.get(username = 'TestAuthorCase2')
+        case3 = single_author.objects.get(username = 'TestAuthorCase3')
         self.assertEqual(case1.password, 'TestAuthorCaseTest1')
         self.assertEqual(case2.password, 'TestAuthorCaseTest2')
-
+        self.assertEqual(case3.password, 'TestAuthorCaseTest3')
+        self.assertEqual(case3.github, 'www.github.com')
+        self.assertEqual(case3.display_name, 'test3')
+        
     def test_description_not_exist(self):
         case1 = single_author.objects.get(username = 'TestAuthorCase1')
+        self.assertEqual(case1.github, '')
+        self.assertEqual(case1.display_name, '')
         self.assertEqual(case1.url, '')
-        
