82a4335a1363638d4a03e9e44c6fbb0459224d40
mengjuL991 <jaydenliaomengju@gmail.com>
2022-12-07 20:03:44 -0700

    fix document swagger

--- SocialDistribution/urls.py
+++ SocialDistribution/urls.py
@@ -22,18 +22,23 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
     
-api_view = get_schema_view(
+schema_view = get_schema_view(
     openapi.Info(
-        title="Socialdistribution API",
-        default_version="1.0.0",
-        description="API documentation of APP"
+        title="social distribution API",
+        default_version='v1',
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
     ),
     public=True,
-    permission_classes=(permissions.AllowAny,),
+    permission_classes=[permissions.AllowAny],
 )
 
-
 urlpatterns = [
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     path('service/',include("service.urls")),
     path('',include("login.urls")),
@@ -41,7 +46,5 @@ urlpatterns = [
     path('',include("post.urls")),
     path('',include('me.urls')), 
     path('',include('inbox.urls')), 
-    path('swagger/', api_view.with_ui('swagger',
-                                         cache_timeout=0), name='schema-swagger-ui'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
